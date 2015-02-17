6b9e0234d893265b5e35d8262f21fc00f72624f4
yinlosky <yinlosky@yahoo.com>
2015-02-17 12:39:55 -0500

    ready for submission

---
+++ Run.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor1.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor2.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor3.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor4.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor5.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor6.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor7.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor8.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor9.txt 2>&1
+#java -jar myKmeans.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 > 10logfor10.txt 2>&1
+
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor1.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor2.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor3.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor4.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor5.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor6.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor7.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor8.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor9.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 5 yes > 5logfor10.txt 2>&1
---
+++ SecondRun.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor1.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor2.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor3.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor4.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor5.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor6.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor7.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor8.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor9.txt 2>&1
+java -jar myKmeansWithOptions.jar ./MNIST/mnist_data.txt ./MNIST/mnist_labels.txt 10 no > S10logfor10.txt 2>&1
